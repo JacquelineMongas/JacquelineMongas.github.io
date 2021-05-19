96043497233ce760f9815ea7c7b31f556121b397
JacquelineMongas <jkshilla@gmail.com>
2021-05-19 23:36:53 +0200

    BOM

--- lesson5/bom/bom.js
+++ lesson5/bom/bom.js
@@ -1,3 +1,5 @@
 const list = document.querySelector('ul');
 const input = document.querySelector('list');
 const button = document.querySelector('button');
+
+button.addEventListener('click', function() { })
