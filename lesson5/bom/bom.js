380babc237423a98249654ecc27063705b9302c7
JacquelineMongas <jkshilla@gmail.com>
2021-05-20 15:58:29 +0200

    BOM

--- lesson5/bom/bom.js
+++ lesson5/bom/bom.js
@@ -1,5 +1,20 @@
-const list = document.querySelector('ul');
-const input = document.querySelector('list');
+const fav = document.querySelector('#fav');
 const button = document.querySelector('button');
+const output = document.querySelector('.list');
 
-button.addEventListener('click', function() { })
+button.addEventListener('click',() => {
+    if(fav.value != ''){
+        // create elements in the list//
+        let li = document.createElement('li');
+        let deletebutton = document.createElement('button');
+        // change some properties... context//
+        li.textContent = fav.value;
+        deletebutton.textContent = '';
+        deletebutton.textContent = 'delete';
+// ...add the button to the li//
+li.append(deletebutton);
+output.append(li);
+
+
+    }
+ });
