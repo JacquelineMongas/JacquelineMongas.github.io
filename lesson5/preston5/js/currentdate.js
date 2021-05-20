42515a651eef62486d1f17c5d5b4315717bf409a
JacquelineMongas <jkshilla@gmail.com>
2021-05-20 23:51:40 +0200

    Week 5

---
+++ lesson5/preston5/js/currentdate.js
@@ -0,0 +1,35 @@
+//day names arrays
+const dayNames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday", 
+    "Friday", 
+    "Saturday"
+    ];
+
+// long months names arrays
+const monthNames = [
+    "January",
+    "Febrary",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+const todaysDate = new Date();
+const dayName = dayNames[todaysDate.getDay()];
+const monthName = monthNames[todaysDate.getMonth()];
+const currentDate = `${dayName}, ${todaysDate.getDate()} ${monthName}, ${todaysDate.getFullYear()}`;
+
+document.getElementById('lastUpdate').textContent = currentDate;
+document.querySelector('#rigthsYear').textContent = `${todaysDate.getFullYear()},`;
+document.getElementById('modifiedTime').textContent = `${document.lastModified}`;
