7596dfd9f4ce623b365e3e05e2a84b69accd7b04
JacquelineMongas <jkshilla@gmail.com>
2021-05-22 03:04:12 +0200

    Week 5

---
+++ lesson5/preston5/js/banner.js
@@ -0,0 +1,15 @@
+const dayNames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday", 
+    "Friday", 
+    "Saturday"
+    ]
+
+let date = newDate();
+console.log(thedate.getDate());
+if (thedate.getDate()==5){
+    document.querySelector('#prestonPancakes').style.display='block';
+}
