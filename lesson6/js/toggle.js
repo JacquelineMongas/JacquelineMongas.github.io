95252749b34dd39fbd656daa495ddcb2e6c48ded
JacquelineMongas <jkshilla@gmail.com>
2021-05-27 00:27:32 +0200

    week 6

--- lesson6/js/toggle.js
+++ lesson6/js/toggle.js
@@ -1,7 +1,21 @@
-const hambutton = document.querySelector('.humberger');
-const mainnav = document.querySelector('.navigation')
+const hamButton = document.querySelector('.ham');
+const mainMenu = document.querySelector('.navigation');
 
-hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+hamButton.addEventListener('click', () =>{mainMenu.classList.toggle('responsive')}, false);
 
-// To solve the mid resizing issue with responsive class on
-window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+window.onresize = () => {
+    if(window.innerWidth > 760){
+        mainMenu.classList.remove('responsive');
+    }
+}
+
+var currentDate = new Date();
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+document.getElementById("output").innerHTML = days[currentDate.getDay()] + ", " + currentDate.getDate() + " " + months[currentDate.getMonth()] + " " + currentDate.getFullYear();
+
+//preston page - adds pancake reminder on fridays
+if (currentDate.getDay() == 5) {
+    document.getElementById("prestonPancakes").style.display = "block";
+}
