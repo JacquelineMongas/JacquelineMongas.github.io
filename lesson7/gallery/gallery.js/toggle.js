c4c0aaaa93c24f81b8570b938003088e8ca71e3f
JacquelineMongas <jkshilla@gmail.com>
2021-06-05 23:28:27 +0200

    corrections

--- lesson7/gallery/gallery.js/toggle.js
+++ lesson7/gallery/gallery.js/toggle.js
@@ -9,8 +9,3 @@ window.onresize = () => {
     }
 }
 
-var currentDate = new Date();
-var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
-document.getElementById("output").innerHTML = days[currentDate.getDay()] + ", " + currentDate.getDate() + " " + months[currentDate.getMonth()] + " " + currentDate.getFullYear();
