9b7de7c782a98858aceec6b1e6cbc97cccecddeb
JacquelineMongas <jkshilla@gmail.com>
2021-06-16 14:25:05 +0200

    corrections prophet js

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -4,6 +4,7 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (jsonObject) {
+    console.table(jsonObject);
     const prophets = jsonObject['prophets'];
     
     for (let i = 0; i < prophets.length; i++ ) {
@@ -11,19 +12,17 @@ fetch(requestURL)
       let h2 = document.createElement('h2');
       let p1 = document.createElement('p');
       let p2 = document.createElement('p');
-      let image = document.createElement('image');
+      let image = document.createElement('img');
 
 
 h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
 card.appendChild(h2);
-p1.textContent = 'Date of Birth ' + prophets[i].lastname;
+p1.textContent = 'Date of Birth ' + prophets[i].birthdate;
 card.appendChild(1);
-p2.textContent = 'Date of Birth ' + prophets[i].lastname;
+p2.textContent = 'Place of Birth ' + prophets[i].birthplace;
 card.appendChild(2);
 
 image.setAttribute('src', prophets[i].imageurl);
-card.appendChild(image);
-
 image.setAttribute('src', prophets[i].name + ' ' + prophets[i].lastname + ' - ' + (i + 1));
 card.appendChild(image);
 
