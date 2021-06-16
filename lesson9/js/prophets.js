39112103d56ff4a3dfc38b0df8dcd1e58c5acb45
JacquelineMongas <jkshilla@gmail.com>
2021-06-17 00:31:42 +0200

    Lesson 9 Later-day Prophets Card

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -10,21 +10,20 @@ fetch(requestURL)
     for (let i = 0; i < prophets.length; i++ ) {
       let card = document.createElement('section');
       let h2 = document.createElement('h2');
-      let p1 = document.createElement('p');
-      let p2 = document.createElement('p');
+      let bDay = document.createElement('p');
+      let bPlace = document.createElement('p');
       let image = document.createElement('img');
-
-
-h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-card.appendChild(h2);
-p1.textContent = 'Date of Birth ' + prophets[i].birthdate;
-card.appendChild(1);
-p2.textContent = 'Place of Birth ' + prophets[i].birthplace;
-card.appendChild(2);
-
-image.setAttribute('src', prophets[i].imageurl);
-image.setAttribute('src', prophets[i].name + ' ' + prophets[i].lastname + ' - ' + (i + 1));
-card.appendChild(image);
+  
+      image.setAttribute('src', prophets[i].imageurl);
+      image.setAttribute('alt', `${prophets[i].name} ${prophets[i].lastname} - ${prophets[i].order}`);
+      h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+      bDay.textContent = 'Birthdate: ' + prophets[i].birthdate;
+      bPlace.textContent = 'Birthplace: ' + prophets[i].birthplace;
+  
+      card.appendChild(h2);
+      card.appendChild(bDay);
+      card.appendChild(bPlace);
+      card.appendChild(image);
 
 document.querySelector('div.cards').appendChild(card);
 
