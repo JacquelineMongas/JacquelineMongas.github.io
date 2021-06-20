f34fcd5e56eb808b56dd9f36166fdc7daab581a2
JacquelineMongas <jkshilla@gmail.com>
2021-06-20 02:53:32 +0200

    lesson 9 home page

--- js/toggle.js
+++ js/toggle.js
@@ -1,10 +1,9 @@
-function toggleMenu() {
-    var temp = document.getElementsByClassName('toggler')[0]
-    temp.classList.toggle('hide')
-    if (temp.classList[1]=='hide') {
-        document.querySelector('.toggler > a').innerHTML = '&#9776; Menu'
-    }
-    else {
-        document.querySelector('.toggler > a').innerHTML = '&#10006; Close'
-    }
-}
+window.addEventListener('load', () => {
+    const hambutton = document.querySelector('.ham');
+    const mainnav = document.querySelector('#navigation')
+
+    hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+    // To resolve the mid resizing issue with class on
+    window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+})
