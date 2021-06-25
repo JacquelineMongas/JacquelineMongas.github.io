ac59a6c2ef39384b61d99b0f369a20891ce99c85
JacquelineMongas <jkshilla@gmail.com>
2021-06-25 14:16:10 +0200

    week 10

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,10 +1,14 @@
-const apiURL = 'http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=cf458bbc49198210ac08ee36c73ee078';
+const cityid = "5604473";
+const APPID = "cf458bbc49198210ac08ee36c73ee078";
+
+const apiURL = `https://api.openweathermap.org/data/2.5/weather?id=${cityid}&units=imperial&appid=${APPID}`;
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
     const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
 const desc = jsObject.weather[0].description;  // note how we reference the weather array
+
 document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
 document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
 document.getElementById('icon').setAttribute('alt', desc);
