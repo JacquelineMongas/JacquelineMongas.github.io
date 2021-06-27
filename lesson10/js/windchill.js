72f6f9d25b6a6e741ddf301729ef20ee14263dbb
JacquelineMongas <jkshilla@gmail.com>
2021-06-27 03:02:22 +0200

    lesson 10 commit

--- lesson10/js/windchill.js
+++ lesson10/js/windchill.js
@@ -1,19 +1,30 @@
+
+const apiURL = `https://api.openweathermap.org/data/2.5/weather?id=$5604473&units=imperial&appid=$cf458bbc49198210ac08ee36c73ee078`;
 fetch(apiURL)
  
 .then((response) => response.json())
   .then((jsObject) => {
-    console.log(jsObject);
-    const tempF = document.querySelector("#currentTemp").textContent;
-    const speed = document.querySelector("#windSpeed").textContent;
-    
-    if (tempF <= 50 && speed >= 3.0) {
-      let f =
-        35.74 +
-        0.6215 * tempF -
-        35.75 * speed ** 0.16 +
-        0.4275 * tempF * speed ** 0.16;
-      document.querySelector("#windChill").textContent = f.toFixed(0)+"&deg;F";
-    } else {
-      document.querySelector("#windChill").textContent = "N/A";
-    }
-  });
+    //console.log(jsObject);
+    document.getElementById('.weatherSummary.temperature').textContent = jsObject.main.temp;
+    document.getElementById('.weatherSummary.currentTemp').textContent = jsObject.weather[0].description;
+    document.getElementById('.weatherSummary.humidity').textContent = jsObject.main.humidity;
+    document.getElementById('.weatherSummary.windSpeed').textContent = jsObject.wind.speed;
+      //Windchill Calc
+      //INPUT 
+      let temperature = parseFloat(jsObject.main.temp);
+      let windSpeed = parseFloat(jsObject.windSpeed);
+      //PROCESSING
+      let windchill = "N/A";
+      if (temperature <= 50 && windSpeed > 3) {
+          windchill = windChill(temperature, windSpeed) + "&deg;F";
+      }
+      //OUTPUT
+      document.getElementById("windChill").innerHTML = windchill;
+      //windchill calculation function
+      function windChill(tempF, speed) {
+          windchill = 35.74 + (0.6215 * tempF) - (35.75 * Math.pow(speed, .16)) + (0.4275 * tempF * Math.pow(speed, .16));
+          return windchill.toFixed(0);
+      }
+
+});
+
