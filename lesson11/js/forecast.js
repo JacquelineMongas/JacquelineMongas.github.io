b92e6a34bf7d91a6dbd060d454dc0716e55a448d
JacquelineMongas <jkshilla@gmail.com>
2021-06-28 11:57:42 +0200

    lesson 11

---
+++ lesson11/js/forecast.js
@@ -0,0 +1,60 @@
+const cityid = "5604473";
+const APPID = "cf458bbc49198210ac08ee36c73ee078";
+
+const apiURL = `https://api.openweathermap.org/data/2.5/forecast?id=${cityid}&units=imperial&appid=${APPID}`;
+
+//Weather Forecast//
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    const dayOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+    let newList = jsObject.list.filter(x => x.dt_txt.includes("18:00:00"));
+    
+    for (let day = 0; day <= 4; day ++) {
+        let d = new Date(newList[day].dt_txt);
+        document.getElementById(`dayWeek${day+1}`).textContent = dayOfWeek[d.getDay()];
+        document.getElementById(`forecast${day+1}`).textContent = newList[day].main.temp.toFixed(0);
+
+        const imgalt = newList[day].weather[0].description;
+        const imagesrc = 'https://openweathermap.org/img/wn/' + newList[day].weather[0].icon + '@2x.png';
+        document.getElementById(`icon${day+1}`).setAttribute('src', imagesrc);
+        document.getElementById(`icon${day+1}`).setAttribute('alt', imgalt);
+    }
+
+
+  });
+
+
+  //windchills//
+  fetch(apiURL)
+
+  .then((response) => response.json())
+  .then((jsObject) => {
+    //console.log(jsObject);
+    document.getElementById('.weatherSummary.temperature').textContent = jsObject.main.temp;
+    document.getElementById('.weatherSummary.currentTemp').textContent = jsObject.weather[0].description;
+    document.getElementById('.weatherSummary.humidity').textContent = jsObject.main.humidity;
+    document.getElementById('.weatherSummary.windSpeed').textContent = jsObject.wind.speed;
+      //Windchill Calc
+      //INPUT 
+      let temperature = parseFloat(jsObject.main.temp);
+      let windSpeed = parseFloat(jsObject.windSpeed);
+      //PROCESSING
+      let windchill = "N/A";
+      if (temperature <= 50 && windSpeed > 3) {
+          windchill = windChill(temperature, windSpeed) + "&deg;F";
+      }
+      //OUTPUT
+      document.getElementById("windChill").innerHTML = windchill;
+      //windchill calculation function
+      function windChill(tempF, speed) {
+          windchill = 35.74 + (0.6215 * tempF) - (35.75 * Math.pow(speed, .16)) + (0.4275 * tempF * Math.pow(speed, .16));
+          return windchill.toFixed(0);
+      }
+
+});
+
+  
+
+  
